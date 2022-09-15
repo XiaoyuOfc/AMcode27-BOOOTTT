db3dac5d9859f95d1c5561d72badbfe8891f9190
AMcode27 <107024251+adhebawor@users.noreply.github.com>
2022-09-16 02:13:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -30,26 +30,26 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 /*============== SOCIAL ==============*/
 
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
-global.sig = 'https://instagram.com/_ctzhid'
-global.sgh = '-' //github
-global.sgc = 'https://chat.whatsapp.com/Iu9I1k8FnXW1sXT41XIF50' //group whatsapp
-global.sdc = '-' //discord
-global.snh = 'https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow' // my youtube channel
+global.sig = 'https://instagram.com/adhebawor27'
+global.sgh = 'https://github.com/adhebawor/' //github
+global.sgc = 'https://chat.whatsapp.com/FX80Sfkupmk1BVO7mfGhRw' //group whatsapp
+global.sdc = 'https://discord.gg/wPKuMwkf' //discord
+global.snh = 'https://youtube.com/c/JasBiruLangit?sub_confirmation=1' // my youtube channel
 
 /*============== PAYMENT ==============*/
-global.pdana = '082127487538' //dana
-global.povo = '082127487538' //ovo
-global.pgopay = '082127487538' //gopay
-global.plinkaja = '082127487538' //link aja
-global.ppulsa = '082127487538' //telkomsel
-global.ppulsa2 = '082127487538' // kalau ada kartu lain isi aja
-global.psaweria = 'https://saweria.com/mrw4h1d'
+global.pdana = '085211112701' //dana
+global.povo = '085211112701' //ovo
+global.pgopay = '085211112701' //gopay
+global.plinkaja = '085211112701' //link aja
+global.ppulsa = '085211112701' //telkomsel
+global.ppulsa2 = '08213037447' // kalau ada kartu lain isi aja
+global.psaweria = 'https://saweria.com/amcode27'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '6282159957081'
-global.nomorown = '6282127487538'
-global.namebot = 'WH-MODS-BOT-V1'
-global.nameown = 'WH MODS DEV'
+global.nomorbot = '6281277875182'
+global.nomorown = '6282138037447'
+global.namebot = 'AMcode27-BOT'
+global.nameown = 'AdheBawor'
 
 /*============== APIKEY SIMPEL ==============*/
 global.zenzkey = 'b8040941f7'
@@ -58,10 +58,10 @@ global.lolkey = '8e66d0934cf741bfd2182c16' // PUNYA WAHID 357d3c85079c4e0818731e
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6282127487538', 'WH MODS DEV', true],
-  ['6282159957081', 'WH-MODS-BOT-V1', true],
-  ['6288298925386', 'WH-MODS-BOT-V2', true],
-  ['79917357842', 'WH-MODS-BOT-V3', true] //Ganti jd nomormu sama Namamu
+  ['6282138037447', 'AMcode27', true],
+  ['6285211112701', 'AdheBawor', true],
+  ['6281277875182', 'AMcode27-BOT', true],
+  ['6285877009255', 'Nche_yah', true] //Ganti jd nomormu sama Namamu
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -109,21 +109,21 @@ global.APIKeys = { // APIKey Here
 // Kata APIKEY itu isi dengan apikey lu sendiri, dengan beli ke website itu
 
 /*============== WATERMARK ==============*/
-global.wm = '                「 WH-MODS-BOT-V1 」' //Main Watermark
-global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘WH-MODS-BOT-V1 ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
-global.wm3 = '⫹⫺ WH-MODS-BOT-V1 '
-global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
+global.wm = '                「 𝒜𝑀𝒸𝑜𝒹𝑒𝟤𝟩-𝐵𝒪𝒯 」' //Main Watermark
+global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘𝒜𝑀𝒸𝑜𝒹𝑒𝟤𝟩-𝐵𝒪𝒯 ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
+global.wm3 = '⫹⫺ 𝒜𝑀𝒸𝑜𝒹𝑒𝟤𝟩-𝐵𝒪𝒯 '
+global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝓙𝔞𝐦: ${wktuwib}`
 global.bottime = `T I M E : ${wktuwib}`
-global.titlebot = `⫹⫺ WhatsApp Bot | By WH-MODS-DEV`
-global.author = '                「 WH-MODS-BOT-V1  あ」'
+global.titlebot = `⫹⫺ WhatsApp Bot | ɮʏ : ǟɖɦɛ ɮǟաօʀ`
+global.author = '                「 𝒜𝑀𝒸𝑜𝒹𝑒𝟤𝟩-𝐵𝒪𝒯  あ」'
 
 
 /*============== LOGO ==============*/
-global.thumb = 'https://i.ibb.co/XZrK6yQ/transformers.jpg' //Main Thumbnail
-global.imagebot = 'https://i.ibb.co/XZrK6yQ/transformers.jpg'
+global.thumb = 'https://i.ibb.co/BfpRd6c/logo.jpg' //Main Thumbnail
+global.imagebot = 'https://i.ibb.co/BfpRd6c/logo.jpg'
 global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
-global.thumbs = ['https://i.ibb.co/XZrK6yQ/transformers.jpg']
-global.fla = 'https://i.ibb.co/XZrK6yQ/transformers.jpg'
+global.thumbs = ['https://i.ibb.co/BfpRd6c/logo.jpg']
+global.fla = 'https://i.ibb.co/BfpRd6c/logo.jpg'
 global.flaaa2 = [
 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
@@ -159,10 +159,10 @@ global.dxlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.shee
 global.dpdf = 'application/pdf'
 global.drtf = 'text/rtf'
 
-global.thumbdoc = 'https://telegra.ph/file/6e45318d7c76f57e4a8bd.jpg'
+global.thumbdoc = 'https://telegra.ph/file/d15b754647a56ccf4be0d.png'
 
 /*=========== FAKE SIZE ===========*/
-global.fsizedoc = '99999999999999' // default 10TB
+global.fsizedoc = '99999999999999' // default 100TB
 global.fpagedoc = '999'
 
 /*=========== HIASAN ===========*/
@@ -189,8 +189,8 @@ global.htjava = '⫹⫺'    //hiasan Doang :v
 global.hsquere = ['⛶','❏','⫹⫺']
 
 /*============== STICKER WM ==============*/
-global.stickpack = 'WH-MODS-BOT-V1'
-global.stickauth = `By ©WH-MODS-DEV`
+global.stickpack = '𝒜𝑀𝒸𝑜𝒹𝑒𝟤𝟩-𝐵𝒪𝒯'
+global.stickauth = `ɮʏ : ǟɖɦɛ ɮǟաօʀ`
 
 global.multiplier = 38 // The higher, The harder levelup
 
