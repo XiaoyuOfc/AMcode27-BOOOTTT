dc3726f04ceb9f3b5362007f2a2bff5a67062f57
GansKatriel <112304004+GansKatriel@users.noreply.github.com>
2022-09-16 16:55:00 +0800

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,20 +2,19 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://youtube.com/c/JasBiruLangit?sub_confirmation=1*
+let str = *
 
 Pengen mentahan nya ? Langsug Chat Owner aja
 Pengen yang udah di recode oleh owner?
 Script bot ini dijual bila kalian mau bisa chat owner
 
-wa.me/628238037447`
-let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
+wa.me/6282
+let wibu = `https://api-reysem/cosplay
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://youtube.com/c/JasBiruLangit?sub_confirmation=1",
     mediaType: "VIDEO",
-    description: "https://youtube.com/c/JasBiruLangit?sub_confirmation=1", 
-    title: 'AMcode27-BOT By : AdheBawor',
+    description: "https://youtube.com/
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
