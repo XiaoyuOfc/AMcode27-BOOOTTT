c52321b0ccc47d1b530876e1f7e4bc54f5b109a6
AMcode27 <107024251+adhebawor@users.noreply.github.com>
2022-09-16 10:56:46 +0700

    Update info-tqto.js

--- plugins/info-tqto.js
+++ plugins/info-tqto.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m) => {
-    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/8f24dd81957b7f5b990cb.jpg')
     //let wibu = `https://api.zacros.my.id/randomimg/loli`
     let res = await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')
     let txt = await res.text()
@@ -12,64 +12,32 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     let user = global.db.data.users[who]
-let tqto = `*▸ - - - —「 BIG THANKS TO 」— - - - ◂*
-*❉ Adiwajshing:*
-https://github.com/adiwajshing
-*❉ Nurutomo:*
-https://github.com/Nurutomo
-*❉ Istikmal:* 
-https://github.com/BochilGaming
-*❉ Ariffb:*
-https://github.com/Ariffb25
-*❉ Ilman:*
-https://github.com/ilmanhdyt
-*❉ Amirul:*
-https://github.com/amiruldev20
-*❉ Rasel:*
-https://github.com/raselcomel
-*❉ Fatur:*
-https://github.com/Ftwrr
-*❉ Rominaru:*
-https://github.com/Rominaru
-*❉ Kannachann:*
-https://github.com/Kannachann
-*❉ The.sad.boy01:*
-https://github.com/kangsad01
-*❉ Ameliascrf:*
-https://github.com/Ameliascrf
-*❉ Fokus ID:*
-https://github.com/Fokusdotid
-*❉ Johannes:*
-https://github.com/Johannes2803
-*❉ BrunoSobrino:*
-https://github.com/BrunoSobrino
-*❉ Krisna:*
-https://github.com/NevtBotz
-*❉ LitRHap:*
-https://github.com/LitRHap
-*❉ Rlxfly:*
-https://github.com/Rlxfly
-*❉ Aine:*
-https://github.com/Aiinne
-*❉ Papah-Chan:*
-https://github.com/FahriAdison
-*▸ - - - —「 Penulis Ulang 」— - - - ◂*
-*❉ ImYanXiao:*
-https://github.com/ImYanXiao
-*▸ - - - —「 Thanks For Suhu 」— - - - ◂*
-*❉ WH MODS DEV:*
-*❉ Kannachan:*
-*❉ Papah-Chan:*
-*❉ Johannes:*
-*❉ Krisna:*
-*❉ Rlxfly:*
+let tqto = `*▸ - - - -- — _「 Saya 」_ - — - - - ◂*
+*▸ ----「❉Ade Muadibul Muarif❉」----◂*
+*▸ - - - —「 BIG THANKS TO 」— - - - ◂*
+*❉ Alloh SWT*
+*❉ Kedua Orangtua Saya*
+*❉ Semua Keluarga Saya*
+*❉ Teman Hidup Saya(@Nche_yah)*
+*❉ Seluruh Dosen Kampus STMIK Komputama Majenang*
+*❉ Rekan Kampus STMIK Komputama Majenang*
+*❉ Rekan Kampus STMIK Komputama Majenang*
+*❉ Dan Seluruh Rekan yang Mengenali saya*
+    
+*▸ - - - —「 Thanks For Suhu BOT 」— - - - ◂*
+*❉ WH MODS DEV*
+*❉ Kannachan*
+*❉ Papah-Chan*
+*❉ Johannes*
+*❉ Krisna*
+*❉ Rlxfly*
 
 `
 conn.sendButtonDoc(m.chat, tqto, wm,'Thanks','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://youtube.com/c/JasBiruLangit?sub_confirmation=1",
     mediaType: "VIDEO",
-    description: "https://Instagram.com/_ctzhid", 
-    title: 'WH-MODS-DEV-MultiDevice',
+    description: "https://Instagram.com/adhebawor27", 
+    title: 'AMcode27-BOT',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
