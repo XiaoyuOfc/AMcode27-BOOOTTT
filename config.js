89ca666f7f390abdd5c21f9f269889d240cec8b8
abilbotz1 <108676663+abilbotz1@users.noreply.github.com>
2022-10-02 15:50:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -58,10 +58,10 @@ global.lolkey = '8e66d0934cf741bfd2182c16' // PUNYA WAHID 357d3c85079c4e0818731e
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6283840600765', 'RiellXd', true],
+  ['6287868860325', 'RiellXd', true],
   ['6282146218274', 'RiellXd', true],
   ['6282146218274', 'RiellXd', true],
-  ['6283840600765', 'RiellXd', true] //Ganti jd nomormu sama Namamu
+  ['6287868860325', 'RiellXd', true] //Ganti jd nomormu sama Namamu
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
